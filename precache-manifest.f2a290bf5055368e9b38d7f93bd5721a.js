5c1ae348f2e188624927dd5315a8e4c56f3126ed
KCCPMG <connorwales@gmail.com>
2020-07-27 16:39:48 -0700

    Updates

---
+++ precache-manifest.f2a290bf5055368e9b38d7f93bd5721a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bfc42931060311bd4221f1831407ef",
+    "url": "/dnd-attack-comparison/index.html"
+  },
+  {
+    "revision": "013fac7382d25ed23549",
+    "url": "/dnd-attack-comparison/static/css/main.e017d741.chunk.css"
+  },
+  {
+    "revision": "b1098ca2470e6a8e9966",
+    "url": "/dnd-attack-comparison/static/js/2.4b5ffa89.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/dnd-attack-comparison/static/js/2.4b5ffa89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013fac7382d25ed23549",
+    "url": "/dnd-attack-comparison/static/js/main.20665542.chunk.js"
+  },
+  {
+    "revision": "aa0fede6b309f712b622",
+    "url": "/dnd-attack-comparison/static/js/runtime-main.362b4417.js"
+  }
+]);
